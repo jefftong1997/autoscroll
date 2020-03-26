1b590395206c99649f8a833caee998e8581a2f90
jefftong1997 <jeff.tong1997@gmail.com>
2020-03-26 16:53:02 +1100

    removed default files from build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import * as serviceWorker from './serviceWorker';
-//import { isCompositeComponent } from 'react-dom/test-utils';
 
 class App extends React.Component {
     constructor(props) {
@@ -218,8 +215,3 @@ function Contact(props) {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
